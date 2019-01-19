c291bb6ff8acc0ddf07a39de3739981004ef2839
Chris Blackmon <cblackmon@chriss-mbp.attlocal.net>
2019-01-18 18:45:16 -0600

    Added hero component, with placeholder image.

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import logo from '../img/logo.svg'
+
+const Hero = () => (
+    <div className="hero-container">
+        <div className="hero-inner">
+            <img className="hero-image" src="https://picsum.photos/2000/800/?image=555" alt=""/>
+        </div>
+  </div>
+)
+
+export default Hero
