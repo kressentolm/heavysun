9f72a208046bf5b36ca65e8352f2d02417063d37
Chris Blackmon <cblackmon@chriss-mbp.attlocal.net>
2019-01-18 19:05:08 -0600

    WIP - need to update to scss loader, not sass loader.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,10 @@
 import React from 'react'
-// import logo from '../img/logo.svg'
+import Triangle from 'shapes/Triangle';
 
 const Hero = () => (
     <div className="hero-container">
         <div className="hero-inner">
+            <Triangle />
             <img className="hero-image" src="https://picsum.photos/2000/800/?image=555" alt=""/>
         </div>
   </div>
---
+++ src/components/shapes/Triangle.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import logo from '../img/logo.svg'
+
+const Triangle = () => (
+    <div class="triangle"></div>
+)
+
+export default Triangle
