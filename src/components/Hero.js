e3ecdf7c44254aef2eb75995e64d19715873bd32
Chris Blackmon <gossblackmon@gmail.com>
2019-03-13 21:38:41 -0500

    Hero styling and initial "ground work" figured out.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Triangle from 'shapes/Triangle';
+import Square from 'shapes/Square';
 
 const Hero = () => (
     <div className="hero-container">
-        <div className="hero-inner">
-            <Triangle />
-            <img className="hero-image" src="https://picsum.photos/2000/800/?image=555" alt=""/>
+        <div className="hero-inner polyscape-background" id="polyscape-3">
+            <h2 className="hero-title">Hey.</h2>
+            <Square />
         </div>
   </div>
 )
--- src/components/shapes/Triangle.js
+++ src/components/shapes/Triangle.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 const Triangle = () => (
     <div class="triangle"></div>
+    
 )
 
 export default Triangle
